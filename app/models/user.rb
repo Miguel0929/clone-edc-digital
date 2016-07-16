76edba3732476cba7c691071be7f8c9d541f5efe
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-07-15 22:38:27 -0500

    customize invitation views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
-  devise :invitable, :database_authenticatable, :recoverable
+  devise :database_authenticatable, :recoverable, :invitable, :validatable
 
-  validates_presence_of :first_name, :last_name, :phone_number, :password, :password_confirmation
-  validates_confirmation_of :password
-  validates_length_of :password, minimum: 8
+  validates_presence_of :first_name, :last_name, :phone_number
 end
