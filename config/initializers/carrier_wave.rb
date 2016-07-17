ef9c844a608752bc1af8b2108d23939e04249fc0
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-07-17 15:24:12 -0500

    configure fog just production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,14 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:           'Rackspace',
-    rackspace_username: ENV['rackspace_username'],
-    rackspace_api_key:  ENV['rackspace_api_key'],
-    rackspace_region:   :iad,  # optional, defaults to :dfw
-    rackspace_temp_url_key: ENV['rackspace_temp_url_key']#'ciecicapacitacion'
-  }
+  if Rails.env.production?
+    config.fog_credentials = {
+      provider:           'Rackspace',
+      rackspace_username: ENV['rackspace_username'],
+      rackspace_api_key:  ENV['rackspace_api_key'],
+      rackspace_region:   :iad,  # optional, defaults to :dfw
+      rackspace_temp_url_key: ENV['rackspace_temp_url_key']#'ciecicapacitacion'
+    }
 
-  config.fog_directory = ENV['rackspace_fog_directory']
-  config.fog_true = false
+    config.fog_directory = ENV['rackspace_fog_directory']
+    config.fog_public = true
+  end
 end
