605c4e2d6228df6a09222481999acd57bfa40693
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-07-17 15:17:33 -0500

    config fog in production

--- app/assets/javascripts/sortable.js
+++ app/assets/javascripts/sortable.js
@@ -2,9 +2,6 @@ $(document).on('turbolinks:load', function(){
   $('.sortable').sortable({
     axis: 'y',
     placeholder: "sortable-placeholder",
-    handle: '.handle-sort',
-    update: function(){
-      //$.post($(this).data('update-url'), $(this).sortable('serialize'));
-    }
+    handle: '.handle-sort'
   });
 });
---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:           'Rackspace',
+    rackspace_username: ENV['rackspace_username'],
+    rackspace_api_key:  ENV['rackspace_api_key'],
+    rackspace_region:   :iad,  # optional, defaults to :dfw
+    rackspace_temp_url_key: ENV['rackspace_temp_url_key']#'ciecicapacitacion'
+  }
+
+  config.fog_directory = ENV['rackspace_fog_directory']
+  config.fog_true = false
+end
