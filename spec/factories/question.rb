2bd3bcb312405af30435cf2fcfd67a9a72ab061b
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-08-18 16:35:42 -0500

    update factory

--- spec/factories/question.rb
+++ spec/factories/question.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :question do
     question_text  { Faker::Lorem.sentence }
     answer_options { Faker::Lorem.sentence }
+    points { Faker::Number.number(1) }
   end
 end
