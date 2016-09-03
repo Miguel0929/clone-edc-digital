191fa0eb1ea59d9cebb334b80073cbc18d3c4b7f
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-09-03 17:51:58 -0500

    configure google cloud services

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,9 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_credentials = {
-      provider:           'Rackspace',
-      rackspace_username: ENV['rackspace_username'],
-      rackspace_api_key:  ENV['rackspace_api_key'],
-      rackspace_region:   :iad,  # optional, defaults to :dfw
-      rackspace_temp_url_key: ENV['rackspace_temp_url_key']#'ciecicapacitacion'
-    }
+  config.fog_credentials = {
+    provider:           'Google',
+    google_storage_access_key_id: ENV['GOOGLE_STORAGE_ACCESS_KEY'],
+    google_storage_secret_access_key:  ENV['GOOGLE_STORAGE_SECRET'],
+  }
 
-    config.fog_directory = ENV['rackspace_fog_directory']
-    config.fog_public = true
-  end
+  config.fog_directory = ENV['GOOGLE_SEGMENT']
 end
