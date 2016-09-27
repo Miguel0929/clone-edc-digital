d6ca4868e015135a10283cab35b115e46b8c36a7
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-09-27 11:56:46 -0500

    add description and cover to programs

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,7 +1,9 @@
 class Program < ActiveRecord::Base
+  mount_uploader :cover, CoverUploader
+
   has_many :chapters
   has_many :group_programs
   has_many :groups, through: :group_programs
 
-  validates_presence_of :name
+  validates_presence_of :name, :description, :cover
 end
