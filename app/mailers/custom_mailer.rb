d2d321ba8f0cc948c175b09ddb52a8beebb9663f
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-12-01 10:05:07 -0600

    create custom mailers and update views

---
+++ app/mailers/custom_mailer.rb
@@ -0,0 +1,17 @@
+class CustomMailer < Devise::Mailer   
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+
+  def reset_password_instructions(record, token, opts={})
+    Mailjet::Send.create(
+      from_email: "soporte-edcdigital@distritoemprendedor.com",
+      from_name: "Soporte EDCdigital",
+      subject: "Recupera tu contraseña en EDCdigital",
+      "Mj-TemplateID": "75219",
+      "Mj-TemplateLanguage": "true",
+      recipients: [{ 'Email'=> 'passenger@mailjet.com'}],
+      vars: {
+        "confirmation_link" => edit_password_url(record, reset_password_token: token)
+      }
+    )
+  end
+end
