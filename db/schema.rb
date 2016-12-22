b4492f9914ee30a2d9bbcb66cccb0bdcc7c70d1b
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-12-21 20:38:06 -0600

    users evaluate modules

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219221453) do
+ActiveRecord::Schema.define(version: 20161222003718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "access_grants", force: :cascade do |t|
-    t.string   "code"
-    t.string   "access_token"
-    t.string   "refresh_token"
-    t.datetime "access_token_expires_at"
-    t.integer  "user_id"
-    t.integer  "client_id"
-    t.string   "state"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "ahoy_events", force: :cascade do |t|
     t.integer  "visit_id"
@@ -84,14 +72,6 @@ ActiveRecord::Schema.define(version: 20161219221453) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
-
-  create_table "clients", force: :cascade do |t|
-    t.string   "name"
-    t.string   "app_id"
-    t.string   "app_secret"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "comment_notifications", force: :cascade do |t|
     t.integer "comment_id"
@@ -209,6 +189,12 @@ ActiveRecord::Schema.define(version: 20161219221453) do
   add_index "trackers", ["chapter_content_id"], name: "index_trackers_on_chapter_content_id", using: :btree
   add_index "trackers", ["user_id"], name: "index_trackers_on_user_id", using: :btree
 
+  create_table "user_evaluations", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "evaluation_id"
+    t.integer "points"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -241,8 +227,6 @@ ActiveRecord::Schema.define(version: 20161219221453) do
     t.string   "state"
     t.string   "city"
     t.string   "profile_picture"
-    t.string   "provider"
-    t.string   "uid"
   end
 
   add_index "users", ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
