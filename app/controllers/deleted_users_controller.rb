38f36603dfff7edf85cf34f4e6ded63d394f72e1
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2017-01-09 13:02:13 -0600

    add bredcrums

--- app/controllers/deleted_users_controller.rb
+++ app/controllers/deleted_users_controller.rb
@@ -3,7 +3,11 @@ class DeletedUsersController < ApplicationController
   before_action :require_admin
   before_action :set_user, only: [:update]
 
+  add_breadcrumb "EDCDIGITAL", :root_path
+
   def index
+    add_breadcrumb "<a class='active' href='#{deleted_users_path}'>Usuarios desactivados</a>".html_safe
+
     @users = User.includes(:group).only_deleted
   end
 
