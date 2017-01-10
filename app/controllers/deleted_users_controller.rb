fa135c9aa054e4defa0113f4a041b31034e4ced3
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2017-01-09 18:24:38 -0600

    send email after restauration

--- app/controllers/deleted_users_controller.rb
+++ app/controllers/deleted_users_controller.rb
@@ -14,11 +14,31 @@ class DeletedUsersController < ApplicationController
   def update
     @user.restore
 
+    if @user.invitation_accepted_at.nil?
+      send_reactivation_email(90816, @user.email)
+    else
+      send_reactivation_email(90812, @user.email)
+    end
+
     redirect_to deleted_users_path
   end
 
   private
   def set_user
     @user = User.only_deleted.find(params[:id])
+  end
+
+  def send_reactivation_email(template_id, email_address)
+    Mailjet::Send.create(
+      from_email: "soporte-edcdigital@distritoemprendedor.com",
+      from_name: "Soporte EDCdigital",
+      subject: "Tu cuenta de EDCdigital ha sido reactivada",
+      "Mj-TemplateID": template_id,
+      "Mj-TemplateLanguage": "true",
+      recipients: [{ 'Email'=> email_address}],
+      vars: {
+        "user_emailaddress" => email_address
+      }
+    )
   end
 end
