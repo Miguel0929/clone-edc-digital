f422992a5bcc7b0984a96e63cacc1ca90d80d993
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2017-02-08 18:07:01 -0600

    create relation to comments and questions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :answer
+  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  belongs_to :question
 
   validates_presence_of :content
   after_create :create_notification
