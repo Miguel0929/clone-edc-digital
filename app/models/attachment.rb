810b300cc9604703b2a587850873f6f7ca9ff45b
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2017-03-25 14:47:16 -0600

    crud of attachments

---
+++ app/models/attachment.rb
@@ -0,0 +1,28 @@
+class Attachment < ActiveRecord::Base
+  enum document_type: [:image, :pdf, :office, :other_document]
+
+  belongs_to :user
+  mount_uploader :file, FileUploader
+
+  validates_presence_of :file
+
+  before_save :rename_file
+
+  def self.document_type_options
+    [['Fotografía o Imagen', 'image'], ['PDF', 'pdf'], ['Documento Office', 'office'], ['Otro', 'other_document']]
+  end
+
+  def humanize_document_type
+    option = self.class.document_type_options.select { |option| option.include? document_type }.flatten
+    option.empty? ? nil : option[0]
+  end
+
+  def display_name
+    File.basename(file.url)
+  end
+
+  private
+  def rename_file
+    self.file = self.file
+  end
+end
