7c77206681fcac8b1862dbafc712440a2746cef0
Miguel Hernandez <mhbyakkoiv@gmail.com>
2017-05-12 16:55:59 -0500

    Correct answer options

--- app/controllers/dashboard/quiz_answers_controller.rb
+++ app/controllers/dashboard/quiz_answers_controller.rb
@@ -1,13 +1,24 @@
 class Dashboard::QuizAnswersController < ApplicationController
   before_action :authenticate_user!
-  before_action :build_question
+  
+  def create
+    raise :exception
+    #params[:answers].each_with_index do |answer, index|
+     # QuizAnswer.create(answer_params(answer))
+    #end
+    #redirect_to quiz_question.quiz
+  end
 
   private
     def build_question
-      @quiz_question = QuizQuestion.find(params[:quiz_question_id] 
+      @quiz_question = QuizQuestion.find(params[:quiz_question_id] )
     end
 
     def build_answer
       @quiz_question.quiz_answers.find_by(user: current_user, quiz_question: @quiz_question) || @quiz_question.quiz_answers.new(user: current_user) 
+    end
+
+    def answers_params(custom_params)
+      ActionController::Parameters.new(custom_params[1]).permit(:user_id, :quiz_question_id, :answer_text)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510232729) do
+ActiveRecord::Schema.define(version: 20170512213426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -309,6 +309,7 @@ ActiveRecord::Schema.define(version: 20170510232729) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.text     "answer_text"
+    t.boolean  "correct"
   end
 
   add_index "quiz_answers", ["quiz_question_id"], name: "index_quiz_answers_on_quiz_question_id", using: :btree
