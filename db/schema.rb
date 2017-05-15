f22af36310868e22e1a783d966ca00f20a69b510
Miguel Hernandez <mhbyakkoiv@gmail.com>
2017-05-15 17:56:24 -0500

    run migraiton

--- db/schema.rb
+++ db/schema.rb
@@ -150,7 +150,6 @@ ActiveRecord::Schema.define(version: 20170512213426) do
   create_table "group_programs", force: :cascade do |t|
     t.integer "group_id"
     t.integer "program_id"
-    t.integer "position"
   end
 
   add_index "group_programs", ["group_id"], name: "index_group_programs_on_group_id", using: :btree
@@ -356,11 +355,9 @@ ActiveRecord::Schema.define(version: 20170512213426) do
     t.string   "reportable_type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.integer  "user_id"
   end
 
   add_index "reports", ["reportable_type", "reportable_id"], name: "index_reports_on_reportable_type_and_reportable_id", using: :btree
-  add_index "reports", ["user_id"], name: "index_reports_on_user_id", using: :btree
 
   create_table "route_covers", force: :cascade do |t|
     t.string   "name"
@@ -526,5 +523,4 @@ ActiveRecord::Schema.define(version: 20170512213426) do
   add_foreign_key "quiz_answers", "users"
   add_foreign_key "quiz_questions", "quizzes"
   add_foreign_key "ratings", "users"
-  add_foreign_key "reports", "users"
 end
