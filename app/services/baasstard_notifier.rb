cbb1847d19a633fada92565ae08d30269c17a630
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2017-05-31 19:27:56 -0500

    push to baasstard

--- app/services/baasstard_notifier.rb
+++ app/services/baasstard_notifier.rb
@@ -2,12 +2,12 @@ require 'net/http'
 
 class BaasstardNotifier
   def self.user_invited(user)
-    uri = URI('https://www.edc-digital.com/api/edc/user_invited')
+    uri = URI('http://baasstard.com/api/edc/user_invited')
     res = Net::HTTP.post_form(uri, { email: user.email })
   end
 
   def self.user_accepted(user)
-    uri = URI('https://www.edc-digital.com/api/edc/user_accepted')
+    uri = URI('http://baasstard.com/api/edc/user_accepted')
     res = Net::HTTP.post_form(uri, { email: user.email })
   end
 end
