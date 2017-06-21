63e3fed929c09f5b591e9bfb5f9520839297b5f2
Miguel Hdz <mhernandez.tics@gmail.com>
2017-06-21 14:35:47 -0500

    Revert "Merge branch 'feture/admin-dashboard' into development"
    This reverts commit b290b6ce90d21cbfe9c0be32becdd5a89fa92922, reversing
    changes made to d612aedf670961b2d6acac5b5180d75ee5b76598.

--- app/models/session.rb
+++ app/models/session.rb
@@ -25,10 +25,6 @@ class Session < ActiveRecord::Base
     self.start.strftime('%Y-%m-%d')
   end
 
-  def day
-    self.start.strftime('%Y-%m-%d')
-  end
-
   def minutes
     TimeDifference.between(start, finish).in_minutes
   end
