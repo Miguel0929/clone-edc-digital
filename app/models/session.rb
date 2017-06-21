c92266af96309926b99938f34f22db7e13fb4f40
Miguel Hdz <mhernandez.tics@gmail.com>
2017-06-21 13:18:40 -0500

    change the visits cahrt

--- app/models/session.rb
+++ app/models/session.rb
@@ -25,6 +25,10 @@ class Session < ActiveRecord::Base
     self.start.strftime('%Y-%m-%d')
   end
 
+  def day
+    self.start.strftime('%W')
+  end
+
   def minutes
     TimeDifference.between(start, finish).in_minutes
   end
