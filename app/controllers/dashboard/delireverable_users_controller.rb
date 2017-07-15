b5209fd9b54c96df575db26489e40653698d5161
Ivan velasquez <ivan.velasquez.rios@gmail.com>
2017-07-15 02:27:37 -0500

    add breadcrums

--- app/controllers/dashboard/delireverable_users_controller.rb
+++ app/controllers/dashboard/delireverable_users_controller.rb
@@ -4,6 +4,9 @@ class Dashboard::DelireverableUsersController < ApplicationController
   add_breadcrumb "EDCDIGITAL", :root_path
 
   def new
+    add_breadcrumb "Entregables", :dashboard_delireverables_path
+    add_breadcrumb "<a href='#{new_dashboard_delireverable_delireverable_user_path(@delireverable)}' class='active'>Subir entregable</a>".html_safe
+
     if delireverable = @delireverable.delireverable_users.find_by(user: current_user)
       @delireverable_user = delireverable
     else
@@ -12,6 +15,9 @@ class Dashboard::DelireverableUsersController < ApplicationController
   end
 
   def create
+    add_breadcrumb "Entregables", :dashboard_delireverables_path
+    add_breadcrumb "<a href='#{new_dashboard_delireverable_delireverable_user_path(@delireverable)}' class='active'>Subir entregable</a>".html_safe
+
     if delireverable = @delireverable.delireverable_users.find_by(user: current_user)
       @delireverable_user = delireverable
       @delireverable_user.file = delireverable_user_params[:file]
