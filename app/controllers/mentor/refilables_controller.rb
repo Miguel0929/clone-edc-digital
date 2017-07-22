23bd4f7dd3abaf545c6cff7f940a5d58f7dbe5bf
Ivan velasquez <ivan.velasquez.rios@gmail.com>
2017-07-21 19:00:22 -0500

    bredcrums for mentor

--- app/controllers/mentor/refilables_controller.rb
+++ app/controllers/mentor/refilables_controller.rb
@@ -2,13 +2,20 @@ class Mentor::RefilablesController < ApplicationController
   before_action :set_student
   before_action :set_refilable
 
+  add_breadcrumb "EDCDIGITAL", :root_path
+  before_action :set_user_breadcrum
+
   def show
+    add_breadcrumb "<a class='active' href='#{mentor_student_refilable_path(@user, @refilable)}'>#{@refilable.template_refilable.name}</a>".html_safe
   end
 
   def edit
+    add_breadcrumb "<a class='active' href='#{edit_mentor_student_refilable_path(@user, @refilable)}'>#{@refilable.template_refilable.name}</a>".html_safe
   end
 
   def update
+    add_breadcrumb "<a class='active' href='#{edit_mentor_student_refilable_path(@user, @refilable)}'>#{@refilable.template_refilable.name}</a>".html_safe
+    
     @refilable.update(refilable_params)
 
     redirect_to mentor_student_path(@user), notice: 'Rellenable actualizado'
@@ -25,5 +32,9 @@ class Mentor::RefilablesController < ApplicationController
 
   def refilable_params
     params.require(:refilable).permit(:comments)
+  end
+
+  def set_user_breadcrum
+    add_breadcrumb "#{@user.name}", mentor_student_path(@user)
   end
 end
