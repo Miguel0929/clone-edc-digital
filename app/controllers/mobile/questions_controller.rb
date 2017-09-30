095845478a5fbe166259915e38f884e892c2e347
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2017-09-30 13:55:29 -0500

    parse answer checkbox

--- app/controllers/mobile/questions_controller.rb
+++ app/controllers/mobile/questions_controller.rb
@@ -5,6 +5,13 @@ class Mobile::QuestionsController < Mobile::BaseController
     question = Question.find(params[:id])
     answer = question.answers.find_by(user: current_user)
 
-    render json: { question: question, answer: answer }
+    parsed_answer = []
+    unless answer.nil?
+      if question.checkbox?
+        parsed_answer = answer.answer_text.split('\n')
+      end
+    end
+
+    render json: { question: question, answer: answer, parsed_answer: parsed_answer}
   end
 end
