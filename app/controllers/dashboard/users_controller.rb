0a36fb4e524867654be91ec79c8b40ec6ad51504
paxo50 <donpancholin50@hotmail.com>
2017-10-11 14:10:05 -0500

    current user en dashboard/users

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -1,10 +1,9 @@
 class Dashboard::UsersController < ApplicationController
   before_action :authenticate_user!
-
   add_breadcrumb "EDCDIGITAL", :root_path
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     add_breadcrumb "<a class='active' href='#{dashboard_user_path(@user)}'>Información de perfil</a>".html_safe
   end
 end
