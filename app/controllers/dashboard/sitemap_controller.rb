acf06944918ce4a547c00397267fb5d4d1390deb
RubenGR <ruben.g.ram@gmail.com>
2017-11-29 14:23:52 -0600

    Revision del merge con la branch del tour

--- app/controllers/dashboard/sitemap_controller.rb
+++ app/controllers/dashboard/sitemap_controller.rb
@@ -1,6 +1,6 @@
 class Dashboard::SitemapController < ApplicationController
   before_action :authenticate_user!
-  add_breadcrumb "EDCDIGITAL", :root_path
+  add_breadcrumb "EDC DIGITAL", :root_path
 
   def index
     add_breadcrumb "<a class='active' href='#{ dashboard_sitemap_index_path }'>Mapa del sitio</a>".html_safe
