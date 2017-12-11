ee204768e2ea81e91e3b3950dd927e38077498c3
paxo50 <donpancholin50@hotmail.com>
2017-12-11 10:25:42 -0600

    comits perdidos

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   include Elasticsearch::Model::Callbacks
   acts_as_token_authenticatable
 
+  after_create :create_code
   attr_accessor :agreement
   acts_as_paranoid
   mount_uploader :profile_picture, ProfilePictureUploader
@@ -38,6 +39,9 @@ class User < ActiveRecord::Base
   belongs_to :industry
   has_many :panel_notifications
   has_many :refilables
+  has_many :attempts
+  has_one :user_code
+
 
   devise :database_authenticatable, :recoverable, :invitable, :validatable, :registerable, :omniauthable
 
@@ -45,6 +49,7 @@ class User < ActiveRecord::Base
   scope :mentors, -> { where(role: 1) }
   scope :staffs, -> { where(role: 3) }
   scope :invitation_accepted, -> { where.not('invitation_accepted_at' => nil) }
+  scope :invitation_no_accepted, -> { where('invitation_accepted_at' => nil) }
   scope :search_query, -> (query) {
     where('lower(users.first_name) LIKE lower(?) OR lower(users.last_name) LIKE lower(?) OR lower(users.email) LIKE lower(?)',
          "%#{query}%", "%#{query}%", "%#{query}%")
@@ -57,6 +62,7 @@ class User < ActiveRecord::Base
 
   before_create :set_origin
   after_create :assign_group
+  after_update :del_code
 
   def self.authenticate(email, password)
     user = find_for_authentication(email: email)
@@ -395,6 +401,29 @@ class User < ActiveRecord::Base
     if mentor?
       group.users << self
     end
+  end
+
+  def create_code
+    if self.user_code.nil?
+      unique = true
+      codigo = SecureRandom.hex(6) 
+      while unique
+        if UserCode.find_by(codigo: codigo).nil?
+          UserCode.create(codigo: codigo, user: self)
+          unique = false
+        else
+          codigo = SecureRandom.hex(6)
+        end
+      end
+    end
+  end
+
+  def del_code
+    unless self.invitation_accepted_at.nil?
+      unless self.user_code.nil?   
+        self.user_code.destroy
+      end  
+    end  
   end
 
 end
