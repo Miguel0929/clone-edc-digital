33d3834c055e83139377e75a9e89547dae959a8c
RubenGR <ruben.g.ram@gmail.com>
2018-02-14 16:02:28 -0600

    Ver mentores desde vista de mentores cuando se busca con elastic search

--- app/controllers/search/users_controller.rb
+++ app/controllers/search/users_controller.rb
@@ -2,7 +2,11 @@ class Search::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :require_admin_or_mentor
 
+  add_breadcrumb "EDC DIGITAL", :root_path
+
   def index
+    add_breadcrumb "<a class='active' href='#{search_users_path}'>Buscar</a>".html_safe
+
     @users = case current_user.role
       when 'admin'
         User.search(params[:search])
