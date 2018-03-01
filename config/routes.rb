cae9c02eb3087cbc5aa4660270d1f2521df3b17a
RubenGR <ruben.g.ram@gmail.com>
2018-03-01 11:58:13 -0600

    Espacio en blanco

--- config/routes.rb
+++ config/routes.rb
@@ -322,6 +322,7 @@ Rails.application.routes.draw do
         get :inactive_students
       end
     end
+    
     resources :sitemap, only: [:index]
     resources :evaluations, only: [:index, :show, :update]
     resources :program_details, only: [:index]
