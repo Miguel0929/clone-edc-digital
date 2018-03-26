46f173e4a228e712c733d817ee168fb8087f1293
RubenGR <ruben.g.ram@gmail.com>
2018-03-26 14:01:52 -0600

    Poniendo gráfica de activos inactivos

--- app/controllers/control_panel_controller.rb
+++ app/controllers/control_panel_controller.rb
@@ -15,6 +15,8 @@ class ControlPanelController < ApplicationController
     @reports = Report.all
     @visits = Visit.where(started_at: 60.day.ago...Time.now)
     @track = timetrack2
+    @actives = User.where(role: 0).where.not(invitation_accepted_at: nil).count
+    @inactives = User.where(role: 0, invitation_accepted_at: nil).count
 
     #@activados = User.where(invitation_created_at: 6.months.ago.to_date..Date.today).where.not(invitation_accepted_at: nil).select('invitation_created_at, count(invitation_created_at) as total')
     #.group(:invitation_created_at).map {|user| [user.invitation_created_at.strftime('%Y-%m-%d'), user.total]}
