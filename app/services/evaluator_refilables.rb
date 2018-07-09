f6a4042c2a86d702153b2f301a81a738b2690068
Ruben <ruben.g.ram@gmail.com>
2018-07-09 12:04:12 -0500

    Evitar crear más de un user evaluation refilable por evaluation refilable

--- app/services/evaluator_refilables.rb
+++ app/services/evaluator_refilables.rb
@@ -15,8 +15,20 @@ class EvaluatorRefilables
     ActiveRecord::Base.transaction do
       points = []
       @evaluations.each do |key, value|
-        user_evaluation = UserEvaluationRefilable.find_or_initialize_by(user: @user, refilable_id: @refilable, evaluation_refilable_id: value[:evaluation])
+        #user_evaluation = UserEvaluationRefilable.find_or_initialize_by(user: @user, refilable_id: @refilable, evaluation_refilable_id: value[:evaluation])
+        #puts "mamar"
+        #puts ["user", @user.id]
+        #puts ["evaluation_refilable_id", value[:evaluation]]
+        #puts ["refilable", @refilable]
+        #puts ["mentor", @mentor]
+        evals = UserEvaluationRefilable.where(user: @user, evaluation_refilable_id: value[:evaluation])
+        if evals.count > 1
+          user_evaluation = evals.order(:created_at).last
+        else
+          user_evaluation = UserEvaluationRefilable.find_or_initialize_by(user: @user, evaluation_refilable_id: value[:evaluation])
+        end
         user_evaluation.points = value[:points]
+        user_evaluation.refilable_id = @refilable # NUEVO
         if user_evaluation.save!
           points.push(user_evaluation.puntaje)
         end
