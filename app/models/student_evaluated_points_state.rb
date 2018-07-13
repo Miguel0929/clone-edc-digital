d909db2f5f28c6b082f24366f33ccefd2f3bc17c
Francisco Salazar <batpaxo@gmail.com>
2018-07-13 18:03:51 -0500

    analitico por estados y general

--- app/models/student_evaluated_points_state.rb
+++ app/models/student_evaluated_points_state.rb
@@ -1,6 +1,5 @@
 class StudentEvaluatedPointsState < ActiveRecord::Base
 	belongs_to :program
 	belongs_to :state
-	enum role: [:evaluation, :refilable, :questions ]
-
+	enum tipo: [:evaluation, :refilable, :questions ]
 end
