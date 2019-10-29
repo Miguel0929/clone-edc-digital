24bdbf5c5922b5ab89895f90cc521257719f32d6
Ruben <ruben.g.ram@gmail.com>
2019-10-29 12:24:28 -0600

    Debuging custom_mailer link 1

--- app/mailers/custom_mailer.rb
+++ app/mailers/custom_mailer.rb
@@ -4,9 +4,19 @@ class CustomMailer < Devise::Mailer
 
   def reset_password_instructions(record, token, opts={})
 
+    puts "record"
+    puts record
+    puts "token"
+    puts token
+    puts "opts"
+    puts opts
+    puts "link"
+    puts edit_password_url(record, reset_password_token: token)
+
     template_title = "Recupera tu contraseña"
     template_name = (record.first_name.nil? ? "Hola" : record.name)
-    template_message = "¿Tuviste problemas para recordar tu contraseña? Sigue el siguiente enlace para recuperarla:</p><table><tr><td align='center'><a href='" + edit_password_url(record, reset_password_token: token) + "'><h4>Cambiar contraseña</h4></a></td></tr><tr></table><p>Continuamos en contacto contigo."
+    #template_message = "¿Tuviste problemas para recordar tu contraseña? Sigue el siguiente enlace para recuperarla:</p><table><tr><td align='center'><a href='" + edit_password_url(record, reset_password_token: token) + "'><h4>Cambiar contraseña</h4></a></td></tr><tr></table><p>Continuamos en contacto contigo."
+    template_message = "token " token.to_s + " record " + token.to_s + " link " + edit_password_url(record, reset_password_token: token).to_s
     template_footer = company_name_helper('Nuestro equipo')
     mail_recipient = record.email
     mail_subject = "Recupera tu contraseña de la plataforma"
