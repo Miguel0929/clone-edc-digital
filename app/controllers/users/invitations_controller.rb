ca2809ed315e313c66ac275c189a73e81ed46f57
Ruben <ruben.g.ram@gmail.com>
2019-10-30 13:07:18 -0600

    Grupo e Institucion en single user invitation 2

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -24,7 +24,7 @@ class Users::InvitationsController < Devise::InvitationsController
       to_group_university = to_group.nil? ? nil : to_group.university
       to_group_university_name = to_group_university.nil? ? "[Institución sin nombre]" : to_group_university.name
 
-      template_title = "¡Es tiempo de iniciar! Perritos"
+      template_title = "¡Es tiempo de iniciar!"
       template_name = ""
       template_group = "Has sido invitado al grupo " + to_group_name + (to_group_university.nil? ? ". " : " vinculado a esta Institución: " + to_group_university_name + ". ")
       template_message = "Te informamos que tu cuenta en " + company_name_helper('nuestra plataforma') + " ha sido creada. " + template_group + "Para comenzar, debes activar tu cuenta dando clic en el siguiente botón: </p> <table width='100%' cellspacing='0' cellpadding='0' border='0'><tbody><tr><td style='padding: 10px 10px 10px 10px' bgcolor: '#f8f8f8' align: 'center'><table cellspacing='0' cellpadding='0' border='0'><tbody><tr><td><a href='" + invitation_link + "' class='button'>ACTIVA TU CUENTA AQUÍ</a></td></tr></tbody></table></td></tr></tbody></table></br><p style='margin-top: 20px;'>En caso de que no logres acceder, puedes copiar la siguiente liga y pegarla en una ventana de tu navegador:</p><a href='" + invitation_link + "' >" + invitation_link + "</a></br></br><p style='margin-top: 20px;'>Si tienes alguna duda o comentario, no dudes en escribirnos a <strong>" + mailer_from_helper('') + "</strong>. Nuestro equipo de atención a clientes enseguida te antenderá."
