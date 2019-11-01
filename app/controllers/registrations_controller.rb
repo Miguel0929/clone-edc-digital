762b7e86bc36d26319f3e3923b95c418fda10e69
Ruben <ruben.g.ram@gmail.com>
2019-11-01 14:34:10 -0600

    Eliminacion de selects relativos a empresa en devise/registrations/edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,22 +10,18 @@ class RegistrationsController < Devise::RegistrationsController
 
 	def update
 		super do
-			@new_ind = resource.industry_id
-
 			if Date.valid_date?(params[:user_detail]['birthdate(1i)'.to_sym].to_i, params[:user_detail]['birthdate(2i)'.to_sym].to_i, params[:user_detail]['birthdate(3i)'.to_sym].to_i)
 				old_birthdate = (params[:user_detail]['birthdate(3i)'.to_sym] + "/" + params[:user_detail]['birthdate(2i)'.to_sym] + "/" + params[:user_detail]['birthdate(1i)'.to_sym]).to_date
 			else
 				old_birthdate = nil
 			end
-			old_situation = params[:user][:situation]
-			old_interest = params[:user][:interest]
 			old_challenge = params[:user][:challenge]
 			old_goal = params[:user][:goal]
 
 			if current_user.user_detail.nil?
-				current_user.create_user_detail(birthdate: old_birthdate, situation: old_situation, interest: old_interest, challenge: old_challenge, goal: old_goal)
+				current_user.create_user_detail(birthdate: old_birthdate, challenge: old_challenge, goal: old_goal)
 			else
-				current_user.user_detail.update(birthdate: old_birthdate, situation: old_situation, interest: old_interest, challenge: old_challenge, goal: old_goal)
+				current_user.user_detail.update(birthdate: old_birthdate, challenge: old_challenge, goal: old_goal)
 			end
 		end
 	end
