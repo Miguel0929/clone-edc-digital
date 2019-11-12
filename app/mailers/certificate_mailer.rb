cc01dd9d04caa8b49a728eff701384185f47cb5c
Ruben <ruben.g.ram@gmail.com>
2019-11-12 13:02:26 -0600

    Capacidad para adjuntar correo 5

--- app/mailers/certificate_mailer.rb
+++ app/mailers/certificate_mailer.rb
@@ -5,8 +5,9 @@ class CertificateMailer
 		template_title = "Todo esfuerzo tiene recompensa"
 		template_name = "Hola"
 		support_mail = ENV['MAILER_SUPPORT'].nil? ? "Dirección no especificada" : ENV['MAILER_SUPPORT']
+		platform_name = ENV['COMPANY_NAME'].nil? ? "nuestra plataforma" : ENV['COMPANY_NAME']
 		#template_message = "Te felicitamos por haber terminado el programa <strong>" + program + "</strong> que pertenece al curso <strong>" + route + "</strong>. Te enviamos el certificado que acredita tu logro:</p><p><a href='" + certificate_link + "'>Visita este enlace para ver tu certificado >></a></p><p>Estamos para servirte, que tengas un excelente día."
-		template_message = "Te felicitamos por haber terminado el programa <strong>" + program + "</strong> que pertenece al curso <strong>" + route + "</strong>. Te adjuntamos el certificado que acredita tu logro a este correo. En caso de que no puedas ver tu certificado comunícate a esta dirección: " + support_mail + ".</p><p>Estamos para servirte, que tengas un excelente día."
+		template_message = "El equipo de <em>" + platform_name + "</em> te felicita por haber terminado el programa <strong>" + program + "</strong> que pertenece al curso <strong>" + route + "</strong>. Te adjuntamos el certificado que acredita tu logro a este correo. En caso de que no puedas ver tu certificado comunícate a esta dirección: " + support_mail + ".</p><p>Estamos para servirte, que tengas un excelente día."
 		template_footer = company_name_helper('Nuestro equipo')
 		mail_recipient = recipient_address
 		mail_subject = "Certificado del programa " + program
